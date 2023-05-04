6285fcdca6423f0704947e5f26ed1b4bf3cb0150
Abner Alain  Gil Garcia <124418451+AbnerAlg@users.noreply.github.com>
2023-05-03 18:00:36 -0600

    Se sube archivo

---
+++ Argumentos/src/main/java/com/mycompany/argumentos/Argumentos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.argumentos;
+
+/**
+ *
+ * @author abner
+ */
+public class Argumentos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
